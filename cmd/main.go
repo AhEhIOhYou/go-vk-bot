1d52b2d50a4a695825a774cfc695cdda85f6c837
ahehiohyou <diman_vladimirov34@mail.ru>
2023-05-07 14:21:36 +0300

    gin added

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	fmt.Println("hello vk api")
+
+	router := gin.Default()
+
+	log.Fatal(router.Run(":8094"))
 }
