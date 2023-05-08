e7153ab3bf8267b519c6a359fa22dde78130a7f9
ahehiohyou <diman_vladimirov34@mail.ru>
2023-05-08 14:36:19 +0300

    edited .env vars

--- pkg/server/infrastructure/repo.go
+++ pkg/server/infrastructure/repo.go
@@ -11,10 +11,10 @@ func NewRepo() (*Repos, error) {
 
 	return &Repos{
 		Vk:   VkRepo{
-			url: os.Getenv("VK_API_VERSION"),
-			accessToken: os.Getenv("VK_API_VERSION"),
+			url: os.Getenv("VK_API_URL"),
+			accessToken: os.Getenv("VK_API_KEY"),
 			version: os.Getenv("VK_API_VERSION"),
-			methodNames: NewVkMethodNames(os.Getenv("VK_API_VERSION")),
+			methodNames: NewVkMethodNames(os.Getenv("VK_API_METHOD_MESSAGE_SEND")),
 		},
 		Nasa: NasaRepo{
 			
